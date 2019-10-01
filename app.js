a4258c16784e777260e17a2c366943ab51f9b19f
Martin Fenner <martin.fenner@datacite.org>
2019-10-01 09:04:58 +0200

    initial commit

---
+++ app.js
@@ -0,0 +1,38 @@
+if (typeof(PhusionPassenger) !== 'undefined') {
+  PhusionPassenger.configure({ autoInstall: false });
+}
+
+const express = require('express');
+const compression = require('compression');
+const morgan  = require('morgan');
+const { ApolloServer } = require('apollo-server-express');
+const { ApolloGateway } = require("@apollo/gateway");
+
+const gateway = new ApolloGateway({
+  serviceList: [
+    { name: 'profiles', url: 'https://api.test.datacite.org/researchers/graphql' },
+    // more services
+  ],
+});
+ 
+const server = new ApolloServer({
+  gateway,
+  subscriptions: false,
+});
+
+let app = express();
+
+// compress responses
+app.use(compression());
+
+// logging
+app.use(morgan('combined'));
+
+server.applyMiddleware({ app });
+
+if (typeof(PhusionPassenger) !== 'undefined') {
+  app.listen('passenger');
+} else {
+  app.listen(3000);
+}
+ 
