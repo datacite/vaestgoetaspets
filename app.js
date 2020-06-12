a3a53f9e22842cdbc049888eefa4572f62dee8a2
Martin Fenner <martin.fenner@datacite.org>
2020-06-12 09:25:01 +0200

    include strapi graphql api

--- app.js
+++ app.js
@@ -9,15 +9,17 @@ const morgan  = require('morgan');
 const { ApolloServer } = require('apollo-server-express');
 const { ApolloGateway } = require("@apollo/gateway");
 
-const PROFILES_URL = process.env.PROFILES_URL || 'https://api.test.datacite.org/profiles/graphql';
-const CLIENT_API_URL = process.env.CLIENT_API_URL || 'https://api.test.datacite.org/client-api/graphql';
-const API_URL = process.env.API_URL || 'https://api.test.datacite.org/api/graphql';
-const RE3DATA_URL = process.env.RE3DATA_URL || 'https://api.test.datacite.org/re3data/graphql';
+const PROFILES_URL = process.env.PROFILES_URL || 'https://api.stage.datacite.org/profiles/graphql';
+const CLIENT_API_URL = process.env.CLIENT_API_URL || 'https://api.stage.datacite.org/client-api/graphql';
+const API_URL = process.env.API_URL || 'https://api.stage.datacite.org/api/graphql';
+const RE3DATA_URL = process.env.RE3DATA_URL || 'https://api.stage.datacite.org/re3data/graphql';
+const STRAPI_URL = process.env.STRAPI_URL || 'https://strapi.stage.datacite.org/graphql';
 
 const gateway = new ApolloGateway({
   serviceList: [
     // { name: 'profiles', url: PROFILES_URL },
     { name: 'client-api', url: CLIENT_API_URL }
+    { name: 'strapi', url: STRAPI_URL }
     // { name: 'api', url: API_URL },
     // { name: 're3data', url: RE3DATA_URL }
     // more services
